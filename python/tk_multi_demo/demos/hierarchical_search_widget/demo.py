3815a8ed8a3b5034cab71e8d97888609200650d3
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2017-04-19 23:46:51 -0400

    wip

--- python/tk_multi_demo/demos/hierarchical_search_widget/demo.py
+++ python/tk_multi_demo/demos/hierarchical_search_widget/demo.py
@@ -12,8 +12,8 @@ import sgtk
 from sgtk.platform.qt import QtGui
 
 # import the global_search_widget module from the qtwidgets framework
-hierarchical_search_widget = sgtk.platform.import_framework(
-    "tk-framework-qtwidgets", "hierarchical_search_widget")
+shotgun_search_widget = sgtk.platform.import_framework(
+    "tk-framework-qtwidgets", "shotgun_search_widget")
 
 # import the task manager from shotgunutils framework
 task_manager = sgtk.platform.import_framework(
@@ -38,27 +38,14 @@ class HierarchicalSearchWidgetDemo(QtGui.QWidget):
         self._bg_task_manager = task_manager.BackgroundTaskManager(self)
 
         # create the widget
-        search_widget = hierarchical_search_widget.HierarchicalSearchWidget(self)
+        self._search_widget = shotgun_search_widget.HierarchicalSearchWidget(self)
 
         # give the search widget a handle on the task manager
-        search_widget.set_bg_task_manager(self._bg_task_manager)
+        self._search_widget.set_bg_task_manager(self._bg_task_manager)
 
-        # # set the entity types to search through (this is also the default dict)
-        # search_widget.set_searchable_entity_types(
-        #     {
-        #         "Asset": [],
-        #         "Shot": [],
-        #         "Task": [],
-        #         # only active users
-        #         "HumanUser": [["sg_status_list", "is", "act"]],
-        #         "Group": [],
-        #         # only active users
-        #         "ClientUser": [["sg_status_list", "is", "act"]],
-        #         "ApiUser": [],
-        #         "Version": [],
-        #         "PublishedFile": [],
-        #     }
-        # )
+        checkbox = QtGui.QCheckBox("Search in current project only")
+        checkbox.setChecked(True)
+        checkbox.toggled.connect(self._on_checkbox_clicked)
 
         # display some instructions
         info_lbl = QtGui.QLabel(
@@ -82,21 +69,40 @@ class HierarchicalSearchWidgetDemo(QtGui.QWidget):
         layout.setSpacing(16)
         layout.addStretch()
         layout.addWidget(info_lbl)
-        layout.addWidget(search_widget)
+        layout.addWidget(checkbox)
+        layout.addWidget(self._search_widget)
         layout.addWidget(self._activated_label)
         layout.addStretch()
 
         # connect the entity activated singal
-        search_widget.entity_activated.connect(self._on_entity_activated)
+        self._search_widget.node_activated.connect(self._on_node_activated)
 
     def destroy(self):
-        """Clean up the object when deleted."""
+        """
+        Clean up the object when deleted.
+        """
         self._bg_task_manager.shut_down()
 
-    def _on_entity_activated(self, entity_type, entity_id, entity_name):
-        """Handle entity activated."""
+    def _on_node_activated(self, entity_type, entity_id, name, path_label, incremental_path):
+        """
+        Handle node activated.
+        """
+        print entity_type, entity_id, name, path_label, incremental_path
+        if entity_type and entity_id:
+            self._activated_label.setText(
+                "<strong>%s</strong> '%s' with id <tt>%s</tt> at '%s' activated" % (
+                    entity_type, name, entity_id, path_label)
+            )
+        else:
+            self._activated_label.setText(
+                "<strong>Folder</strong> '%s' activated" % (name,)
+            )
 
-        self._activated_label.setText(
-            "<strong>%s</strong> '%s' with id <tt>%s</tt> activated" % (
-                entity_type, entity_name, entity_id)
-        )
+    def _on_checkbox_clicked(self, is_checked):
+        """
+        Toggles the search from site level to project level searches.
+        """
+        if is_checked:
+            self._search_widget.set_search_root(sgtk.platform.current_bundle().context.project)
+        else:
+            self._search_widget.set_search_root(None)
