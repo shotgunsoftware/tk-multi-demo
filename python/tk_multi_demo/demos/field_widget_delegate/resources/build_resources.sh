75e3e11d43f264a4a5379e7676d919e12dc45bd1
Ariel Calzada <ariel.calzada@gmail.com>
2022-03-08 06:31:06 -0500

    Adjust build script and replace sg window icon

--- python/tk_multi_demo/demos/field_widget_delegate/resources/build_resources.sh
+++ python/tk_multi_demo/demos/field_widget_delegate/resources/build_resources.sh
@@ -12,6 +12,7 @@
 
 # The path to output all built .py files to:
 UI_PYTHON_PATH=../ui
+PYTHON_BASE="/Applications/Shotgun.app/Contents/Resources/Python"
 
 # Remove any problematic profiles from pngs.
 for f in *.png; do mogrify $f; done
@@ -24,15 +25,15 @@ function build_qt {
     $1 $2 > $UI_PYTHON_PATH/$3.py
 
     # replace PySide imports with tank.platform.qt and remove line containing Created by date
-    sed -i "" -e "s/from PySide import/from tank.platform.qt import/g" -e "/# Created:/d" $UI_PYTHON_PATH/$3.py
+    sed -i $UI_PYTHON_PATH/$3.py -e "s/from PySide import/from shotgun_desktop.qt import/g" -e "/# Created:/d"
 }
 
 function build_ui {
-    build_qt "pyside-uic --from-imports" "$1.ui" "$1"
+    build_qt "${PYTHON_BASE}/bin/python ${PYTHON_BASE}/bin/pyside-uic --from-imports" "$1.ui" "$1"
 }
 
 function build_res {
-    build_qt "pyside-rcc -py3" "$1.qrc" "$1_rc"
+    build_qt "${PYTHON_BASE}/bin/pyside-rcc -py3" "$1.qrc" "$1_rc"
 }
 
 
